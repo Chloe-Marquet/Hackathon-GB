a5ad9933d150be70e7bf552f9bb479c202b8458f
wissem <wissem.ouhib@gmail.com>
2021-12-16 15:44:20 +0100

    modif générale 1

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-alert("hi");
+
+
+
+$(document).ready(function() {
+
+
+
+});
+
+/*function analyse() { alert("hello")
+
+//text = $('p').text();
+
+//alert(text);
+
+lelien=$("#lapage").val();
+alert(lelien);
+
+};*/
+
+
+var analyse = function (){
+	lelien=$("#lapage").val();
+
+  $.ajax(lelien, {
+    success: function(response) {
+    //	var ledom = document.createElement("div");
+   // 	ledom.innerHTML = response;
+     $("#lapagecontent").html(response);
+  
+   alert( $("#lapagecontent").text());
+
+    }
+  }); 
+};
+
