e46a23b7304b99fc6c1133d288220b02190ee37f
wissem <wissem.ouhib@gmail.com>
2021-12-16 19:02:50 +0100

    modif générale 2

--- main.js
+++ main.js
@@ -21,16 +21,52 @@ alert(lelien);
 
 const analyse = function () {
     let url = $("#lapage").val();
+    let iframe = $("#lapagecontent").attr('src', url);
+    $("#lapagecontent").on('load', function() {
+    	var frameContent = $("#lapagecontent").contents().get();
+    	console.log('frame', frameContent);
+    	const nodes = frameContent[0].all;
+    	console.log('nodes', nodes);
 
-    $.ajax(url, {
+ 		const filtered = Array.from(nodes).filter(function(node) {
+         	//console.log('noeud', node);
+
+         	if(node.firstElementChild!=null) {
+         		console.log('node ', node);
+         		console.log('firstElementChild.nodeType', node.firstElementChild.nodeType);
+         		console.log('firstElementChild', node.firstElementChild);
+         	}
+         	
+         	return node.firstChild && node.firstChild.nodeType === 3 && node.firstElementChild === null ;
+         //	return node.firstChild && node.firstChild.nodeType === 3;
+         });
+ 		
+ 		console.log(filtered);
+
+
+
+
+    });
+    
+
+
+/*    $.ajax(url, {
         success: function (response) {
             //	var ledom = document.createElement("div");
             // 	ledom.innerHTML = response;
             $("#lapagecontent").html(response);
 
-            alert($("#lapagecontent").text());
+         // alert($("#lapagecontent").text());
+         const nodes = $("#lapagecontent").contents();
+         console.log('depart', nodes);
+         const filtered = nodes.filter(function(i, node) {
+         	console.log('noeud', node);
+         	return node.firstChild && node.firstChild.nodeType === 3;
+         });
+         console.log('arrivee', filtered);
+
 
         }
-    });
+    }); */
 };
 
